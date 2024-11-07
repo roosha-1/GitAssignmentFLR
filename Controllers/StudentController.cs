26e82ade6789163adf54d4246dcd87513b0d6d24
rosy871 <yalameju@gmail.com>
2024-11-07 13:45:24 +0100

    Changed in Index action, added constructor in studencontroller

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GitAssignmentFLR.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GitAssignmentFLR.Controllers
 {
     public class StudentController : Controller
     {
+        List<Student> students = new List<Student>();
+       
         public IActionResult Index()
         {
+            var std=new Student(1,"roo","roo@roo.roo");
+            students.Add(std);
             return View();
         }
     }
