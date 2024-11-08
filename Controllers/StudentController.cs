ad8d6ff64cd7eebc21bfefd90a442b0829e7e689
rosy871 <yalameju@gmail.com>
2024-11-08 09:41:41 +0100

    added more students list

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -10,7 +10,13 @@ namespace GitAssignmentFLR.Controllers
         public IActionResult Index()
         {
           var std=new Student(1,"roo","roo@roo.roo");
+          var std1=new Student(2,"moo","moo@roo.roo");
+          var std3=new Student(3,"doo","doo@roo.roo");
+          var std2=new Student(4,"loo","loo@roo.roo");
             students.Add(std);
+            students.Add(std1);
+            students.Add(std2);
+            students.Add(std3);
 
 
             return View();
